18424f154afcc53abdeb678fb3834f8adef21bf7
Ashvith20 <mswarna304@gmail.com>
2025-09-08 16:09:27 -0500

    second commit

--- src/core/embeding.py
+++ src/core/embeding.py
@@ -1,8 +1,15 @@
-
+from sentence_transformers import SentenceTransformer
+from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+import os
+load_dotenv()
 class embed_model:
     @staticmethod
-    def sentence_Transfoer():
-        pass
+    def sentence_Transfoer(chunks, model_name="all-MiniLM-L6-v2"):
+        model = SentenceTransformer(model_name)
+        embeddings = model.encode(chunks)
+        return embeddings
+    
 
     # def huggingface embeding
-    pass
+    pa
